ab8b8b7b5c7036b51758a670e43d533790875986
Dan Peltier <danpeltier@gmail.com>
2024-06-29 17:15:19 -0400

    testing 2

--- index.js
+++ index.js
@@ -61,51 +61,48 @@ app.post('/pdp-api*', async function(req, res) {
 });
 
 app.post('/save-recipe-terms', async (req, res) => {
-  res.json({
-    "testing": req.body
-  });
-  // if(req.body.recipeId && req.body.searchTerms) {
-  //   if(req.body.searchTerms.length == 0) {
-  //     // delete file:
-  //   }
-  //   else {
-  //     // save file:
-  //     let newFilePath = 'demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + '/recipe-terms/' + req.body.recipeId + '.json';
-  //     const file = bucket.file(newFilePath);
-  //     if(file) {
-  //       const readableStream = new Readable();
-  //       readableStream.push(JSON.stringify(req.body.searchTerms));
-  //       readableStream.push(null);
-  //       let gcFile = bucket.file(newFilePath);
-  //       readableStream.pipe(gcFile.createWriteStream({
-  //         resumable: false,
-  //         validation: false,
-  //         contentType: 'application/json'
-  //       }))
-  //       .on('error', (error) => {
-  //         res.json({
-  //           "error": "failed to save to bucket: " + error
-  //         });
-  //       })
-  //       .on('finish', async () => {
-  //         res.json({
-  //           success: req.body.recipeId
-  //         });
-  //       });
-  //     }
-  //     else {
-  //       res.json({
-  //         "error": "Unable to create file"
-  //       });
-  //     }
-  //
-  //   }
-  // }
-  // else {
-  //   res.json({
-  //     error: 'invalid payload'
-  //   })
-  // }
+  if(req.body.recipeId && req.body.searchTerms) {
+    if(req.body.searchTerms.length == 0) {
+      // delete file:
+    }
+    else {
+      // save file:
+      let newFilePath = 'demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + '/recipe-terms/' + req.body.recipeId + '.json';
+      const file = bucket.file(newFilePath);
+      if(file) {
+        const readableStream = new Readable();
+        readableStream.push(JSON.stringify(req.body.searchTerms));
+        readableStream.push(null);
+        let gcFile = bucket.file(newFilePath);
+        readableStream.pipe(gcFile.createWriteStream({
+          resumable: false,
+          validation: false,
+          contentType: 'application/json'
+        }))
+        .on('error', (error) => {
+          res.json({
+            "error": "failed to save to bucket: " + error
+          });
+        })
+        .on('finish', async () => {
+          res.json({
+            success: req.body.recipeId
+          });
+        });
+      }
+      else {
+        res.json({
+          "error": "Unable to create file"
+        });
+      }
+
+    }
+  }
+  else {
+    res.json({
+      error: 'invalid payload'
+    })
+  }
 });
 
 app.post('/search-api*', async (req, res) => {
