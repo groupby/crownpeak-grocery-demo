c71a76c77bb2b71023418eb1899d193f7f1e1dc9
Dan Peltier <danpeltier@gmail.com>
2024-07-23 10:10:34 -0400

    created chunks function for large dl 6

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ app.get('/assets/*', function(req, res) {
 
                 var buf = '';
                 let lastEnd = 0;
-                for(let i = 0; i < 440; i ++) {
+                for(let i = 0; i < 420; i ++) {
                   let start = i*1024*1024;
                   let end = i*1024*1024 + 1024*1024 - 1;
                   lastEnd = end;
