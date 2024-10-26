d72dabeb074b43346ce029b61a001b7042d775f1
Dan Peltier <danpeltier@gmail.com>
2024-10-26 18:40:10 -0400

    updated to include megamenu 2

--- index.js
+++ index.js
@@ -651,10 +651,13 @@ app.get('/*', async (req, res) => {
 
           let megaData = await axios.get(`https://cm.sandbox.groupbycloud.com/api/megamenus/demo-megamenu/categories`, megaOptions);
 
-          let visualData = await axios.get(`https://cm.sandbox.groupbycloud.com/api/megamenus/demo-visualmenu/categories`, megaOptions);
+          // let visualData = await axios.get(`https://cm.sandbox.groupbycloud.com/api/megamenus/demo-visualmenu/categories`, megaOptions);
+          //
+          let addedCode = `<div class="megamenu-data invisible">${JSON.stringify(megaData.data)}</div>`;
 
-          let addedCode = `<div class="megamenu-data invisible">${JSON.stringify(megaData.data)}</div><div class="visualmenu-data invisible">${JSON.stringify(visualData.data)}</div>`;
-
+/*
+<div class="visualmenu-data invisible">${JSON.stringify(visualData.data)}</div>
+*/
           formattedPage = formattedPage.replace('</body>',`${addedCode}</body>`);
 
           res.send(formattedPage);
