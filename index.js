a425c6b181bd3c0d8f78a549dec6d266812db3ba
Dan Peltier <danpeltier@gmail.com>
2024-12-09 15:07:13 -0500

    base template

--- index.js
+++ index.js
@@ -15,37 +15,13 @@ require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.json());
 
-const currentDemo = 'grocery-demo';
-
 app.use(favicon(__dirname + '/favicon.png'));
 
 const {Storage} = require('@google-cloud/storage');
-var env = 'live';
+var env = process.env.ENV;
 
 const storage = new Storage('groupby-demos',process.env.GOOGLE_STORAGE);
 const bucketName = 'demos_content';
-
-async function get404() {
-  const bucket = storage.bucket(bucketName);
-  const file = bucket.file('demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + '/404.html');
-
-  return new Promise((resolve, reject) => {
-    let feed = file.createReadStream();
-    var buf = '';
-    feed.on('data', async function(d) {
-      buf += d;
-    }).on('end', async function() {
-      let formattedPage = buf.replace(/\/dev\//g,'\/').replace(/\/live\//g,'\/').replace(/\/Global Assets\//g,'\/global-assets\/').replace(/\/Global%20Assets\//g,'\/global-assets\/');
-      let completedPg = formattedPage;
-      try {
-        completedPg = await addHeaderFooter(formattedPage);
-      } catch(error) {
-        console.log('error getting header/footer');
-      }
-      resolve(completedPg);
-    })
-  });
-}
 
 app.post('/pdp-api*', async function(req, res) {
   let options = {
@@ -60,55 +36,55 @@ app.post('/pdp-api*', async function(req, res) {
   res.json(pdp.data);
 });
 
-app.post('/save-recipe-terms', async (req, res) => {
-  if(req.body.recipeId && req.body.searchTerms) {
-    const bucket = storage.bucket(bucketName);
-    let newFilePath = 'demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + '/recipe-terms/' + req.body.recipeId + '.json';
-    if(req.body.searchTerms.length == 0) {
-      // delete file:
-      await bucket.file(newFilePath).delete();
-      res.json({
-        success: 'deleted'
-      })
-    }
-    else {
-      // save file:
-      const file = bucket.file(newFilePath);
-      if(file) {
-        const readableStream = new Readable();
-        readableStream.push(JSON.stringify(req.body.searchTerms));
-        readableStream.push(null);
-        let gcFile = bucket.file(newFilePath);
-        readableStream.pipe(gcFile.createWriteStream({
-          resumable: false,
-          validation: false,
-          contentType: 'application/json'
-        }))
-        .on('error', (error) => {
-          res.json({
-            "error": "failed to save to bucket: " + error
-          });
-        })
-        .on('finish', async () => {
-          res.json({
-            success: req.body.recipeId
-          });
-        });
-      }
-      else {
-        res.json({
-          "error": "Unable to create file"
-        });
-      }
-
-    }
-  }
-  else {
-    res.json({
-      error: 'invalid payload'
-    })
-  }
-});
+// app.post('/save-recipe-terms', async (req, res) => {
+//   if(req.body.recipeId && req.body.searchTerms) {
+//     const bucket = storage.bucket(bucketName);
+//     let newFilePath = 'gb-pantry/' + env + '/recipe-terms/' + req.body.recipeId + '.json';
+//     if(req.body.searchTerms.length == 0) {
+//       // delete file:
+//       await bucket.file(newFilePath).delete();
+//       res.json({
+//         success: 'deleted'
+//       })
+//     }
+//     else {
+//       // save file:
+//       const file = bucket.file(newFilePath);
+//       if(file) {
+//         const readableStream = new Readable();
+//         readableStream.push(JSON.stringify(req.body.searchTerms));
+//         readableStream.push(null);
+//         let gcFile = bucket.file(newFilePath);
+//         readableStream.pipe(gcFile.createWriteStream({
+//           resumable: false,
+//           validation: false,
+//           contentType: 'application/json'
+//         }))
+//         .on('error', (error) => {
+//           res.json({
+//             "error": "failed to save to bucket: " + error
+//           });
+//         })
+//         .on('finish', async () => {
+//           res.json({
+//             success: req.body.recipeId
+//           });
+//         });
+//       }
+//       else {
+//         res.json({
+//           "error": "Unable to create file"
+//         });
+//       }
+//
+//     }
+//   }
+//   else {
+//     res.json({
+//       error: 'invalid payload'
+//     })
+//   }
+// });
 
 app.post('/search-api*', async (req, res) => {
   let options = {
@@ -129,20 +105,16 @@ app.post('/search-api*', async (req, res) => {
   res.json(search.data);
 });
 
-app.get('/grocery-demo/grocery-demo/assets/*', function(req, res) {
-  if(req.get('host').indexOf('groupby.cloud') == -1) {
-    env = 'dev';
-  }
-
-  let filePath = req.url.replace('main','pantry');
+app.get('/assets/*', function(req, res) {
+  let filePath = req.url;
 
   const bucket = storage.bucket(bucketName);
   let urlPath = filePath.split('/');
-  const file = bucket.file('demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + filePath.split('?')[0]);
+  const file = bucket.file('gb-pantry/' + env + filePath.split('?')[0]);
 
   file.exists(function(err,exists) {
     if(!exists) {
-      res.send('error 404 - ' + 'demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + filePath.split('?')[0]);
+      res.send('error 404 - ' + filePath.split('?')[0]);
     }
     else {
       let parts = filePath.split('.');
@@ -204,7 +176,7 @@ async function getGlbChunk(file, start, end) {
 app.post('/save-pp', async function(req, res) {
   if(req.body.user && req.body.pp) {
     const bucket = storage.bucket(bucketName);
-    const file = 'demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + '/past-purchases/' + req.body.user + '.json';
+    const file = 'gb-pantry/' + env + '/past-purchases/' + req.body.user + '.json';
 
     const readableStream = new Readable();
     readableStream.push(JSON.stringify(req.body.pp));
@@ -237,7 +209,7 @@ app.post('/save-pp', async function(req, res) {
 app.post('/get-pps', async function(req, res) {
   if(req.body.user) {
     const bucket = storage.bucket(bucketName);
-    const file = bucket.file('demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + '/past-purchases/' + req.body.user + '.json');
+    const file = bucket.file('gb-pantry/' + env + '/past-purchases/' + req.body.user + '.json');
 
     file.exists(async function(err,exists) {
       if(!exists) {
@@ -277,105 +249,9 @@ app.post('/get-pps', async function(req, res) {
   }
 });
 
-app.get('/assets/*', function(req, res) {
-  if(req.get('host').indexOf('groupby.cloud') == -1) {
-    env = 'dev';
-  }
-
-  let filePath = req.url.replace('main','pantry');
-
-  const bucket = storage.bucket(bucketName);
-  let urlPath = filePath.split('/');
-  const file = bucket.file('demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + filePath.split('?')[0]);
-
-  file.exists(async function(err,exists) {
-    if(!exists) {
-      res.send('error 404 - ' + 'demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + filePath.split('?')[0]);
-    }
-    else {
-      let parts = filePath.split('.');
-      let ext = '';
-      if(parts.length > 1) {
-        ext = parts[1].split('?')[0];
-      }
-      // css, js
-      // json
-      if(ext == 'css' || ext == 'js') {
-        let feed = file.createReadStream();
-        var buf = '';
-        feed.on('data', function(d) {
-          buf += d;
-        }).on('end', function() {
-          if(ext == 'css') {
-            res.type('css');
-            // console.log('css file');
-          }
-          if(ext == 'js') {
-            res.type('js');
-            // console.log('js file');
-          }
-          res.send(buf);
-        })
-      }
-      else {
-        if(ext == 'json') {
-          let feed = file.createReadStream();
-          var buf = '';
-          feed.on('data', function(d) {
-            buf += d;
-          }).on('end', function() {
-            res.json(buf);
-          })
-        }
-        else {
-          const publicUrl = file.publicUrl();
-          res.redirect(publicUrl);
-        }
-      }
-    }
-  });
-
-});
-
-app.get('/dev/' + currentDemo + '/*', (req, res) => {
-  res.redirect(req.url.replace('/dev',''));
-});
-
-app.get('/live/' + currentDemo + '/*', (req, res) => {
-  res.redirect(req.url.replace('/live',''));
-});
-
-app.get('/recipes-index.json', (req, res) => {
-  if(req.get('host').indexOf('groupby.cloud') == -1) {
-    env = 'dev';
-  }
-
-  const bucket = storage.bucket(bucketName);
-  let urlPath = req.url.split('/');
-  const file = bucket.file('demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + req.url.split('?')[0]);
-
-  file.exists(function(err,exists) {
-    if(!exists) {
-      res.send('error 404 - ' + 'demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + filePath.split('?')[0]);
-    }
-    else {
-      let feed = file.createReadStream();
-      var buf = '';
-      feed.on('data', function(d) {
-        buf += d;
-      }).on('end', function() {
-        res.send(buf);
-      })
-    }
-  });
-});
-
 app.get('/*', async (req, res) => {
-  if(req.get('host').indexOf('groupby.cloud') == -1) {
-    env = 'dev';
-  }
   const bucket = storage.bucket(bucketName);
-  const file = bucket.file('demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + '/homepage.html');
+  const file = bucket.file('gb-pantry/' + env + '/template.html');
 
   file.exists(function(err,exists) {
     if(!exists) {
@@ -387,15 +263,11 @@ app.get('/*', async (req, res) => {
       feed.on('data', async function(d) {
         buf += d;
       }).on('end', async function() {
-        buf = buf.replace('tile-img|[{image}]','tile-img|[{images.0.uri}]').replace('mini-cart-image|[{image}]','mini-cart-image|[{images.0.uri}]').replace('mini-cart-price|{price}','mini-cart-price|{priceInfo.price}').replace('product-card-price|{price,2}','product-card-price|{priceInfo.price,2}')
-        let regex = new RegExp('/' + currentDemo + '/','g');
-        var formattedPage = buf.replace(/\/dev\//g,'\/').replace(/\/live\//g,'\/').replace(regex,'/');
-
         if(req.url.indexOf('/recipe/') != -1) {
           let recUrlParts = req.url.split('/');
           if(recUrlParts.length > 2) {
             let recipeId = recUrlParts[recUrlParts.length - 2];
-            const file2 = bucket.file('demos-5fg5Xq2wWTzhrKKu/' + env + '/' + currentDemo + '/recipe-terms/' + recipeId + '.json');
+            const file2 = bucket.file('gb-pantry/' + env + '/recipe-terms/' + recipeId + '.json');
             file2.exists(function(err,exists2) {
               if(!exists2) {
                 res.send(formattedPage);
