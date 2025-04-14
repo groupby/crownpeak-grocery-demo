c54fe5e515d65696162be5d415f58091849041d9
Dan Peltier <danpeltier@gmail.com>
2025-04-14 09:07:49 -0400

    update node packages

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(cookieParser());
 const Readable = require('stream').Readable;
 const {Translate} = require('@google-cloud/translate').v2;
 const translate = new Translate();
-const multer = require('multer');
-const upload = multer();
 
 require('dotenv').config();
 
