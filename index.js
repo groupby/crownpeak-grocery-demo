1f56d55c07b09c92ccf82fb357943c24799283bc
Dan Peltier <danpeltier@gmail.com>
2026-02-05 13:36:37 -0500

    fixed rezolve sso

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.use(function (req, res, next) {
             <div><img src="https://rezolve.com/wp-content/uploads/2025/02/rezolve-ai-logo.svg" /></div>
             <div>You are not authorized to view this site.</div>
             <div>If you require access to this demo,</div>
-            <div>you may <a href="mailto:presales@groupbyinc.com">contact PreSales</a> to request it.</div>
+            <div>you may <a href="mailto:presales@rezolve.com">contact PreSales</a> to request it.</div>
             <div>&nbsp;</div>
             <div><a href="/">Click here to try again</a></div>
           </body>
@@ -131,7 +131,7 @@ app.use(function (req, res, next) {
           if(decoded.email && decoded.email == 'presales@gmail.com') {
             triggerOK = true;
           }
-          if(decoded.email.indexOf('@groupbyinc.com') != -1 || decoded.permissions.indexOf(process.env.AUTH0_PERMS) != -1) {
+          if(decoded.email.indexOf('@groupbyinc.com') != -1 || decoded.email.indexOf('@rezolve.com') != -1 || decoded.permissions.indexOf(process.env.AUTH0_PERMS) != -1) {
             // login OK
             next();
           }
